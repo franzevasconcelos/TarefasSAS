beb9dbf4de440a7f0080c71197a0ec547202ad9f
Franzé Vasconcelos <>
2018-08-11 18:08:28 -0300

    - Criação de projeto de view - Área de login

---
+++ View/Models/Usuario.cs
@@ -0,0 +1,7 @@
+﻿namespace View.Models {
+    public class Usuario {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+    }
+}
---
+++ View/Util/API.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+
+namespace View.Util {
+    public static class API {
+        public static string EnderecoBase() {
+            return (string) new AppSettingsReader().GetValue("Endereco.API", typeof(string));
+        }
+
+        public static string Login() {
+            return EnderecoBase() + "usuario?login=";
+        }
+
+    }
+}
